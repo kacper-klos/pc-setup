7c3c3240326cfa2a6357b9c4a62a9b0b61c970d0
kacper-klos <kacper.klos@proton.me>
2025-10-02 20:18:19 +0200

    Config files for i3 and kitty

--- install_from_zero.sh
+++ install_from_zero.sh
@@ -2,12 +2,8 @@
 sudo pacman -Syu
 # Download os gui
 sudo pacman -S --needed \
-    i3-wm i3status i3lock dmenu xorg-xinit xterm \
     feh rofi polybar \
     sddm \
     nvidia nvidia-utils nvidia-settings \
-    kitty 
-# Start using sddm to login
-sudo systemctl enable sddm.service
-# Update initramfs
-sudo mkinitcpio -P
+    kitty \
+    qotf-codenewroman-nerd
